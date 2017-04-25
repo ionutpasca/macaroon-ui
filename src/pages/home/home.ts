15caaa4415ccc9ac80df76481d726721c63eb054
Pasca Ionut <pascaionut94@yahoo.com>
2017-04-26 01:12:07 +0300

    Dummy socket communication

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AuthService } from '../../shared/shared';
 import { User } from '../../models/models';
 
+import * as io from 'socket.io-client';
+
 @IonicPage()
 @Component({
 	selector: 'page-home',
@@ -10,6 +12,9 @@ import { User } from '../../models/models';
 })
 export class Home {
 	currentUser: User;
+	socket: any;
+
+	loggedUsers: any;
 
 	constructor(
 		private navCtrl: NavController,
@@ -21,7 +26,19 @@ export class Home {
 		console.log('ionViewDidLoad Home');
 		this.auth.getUserInfo().then(user => {
 			this.currentUser = user;
+			this.socket = io('http://localhost:8083');
+			this.socket.on('give_data', () => {
+				this.socket.emit('user_data', this.currentUser);
+			});
+			
+			this.socket.emit('get_logged_users');
+			this.socket.on('logged_users', (loggedUsers) => {
+				console.log("LOGGED USERS", loggedUsers);
+			});
 		})
+		.catch(err => {
+			console.log('Error getting user info');
+		});
 	};
 
 };
